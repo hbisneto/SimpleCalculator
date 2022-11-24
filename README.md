b9ddfa437e12161386913400ed7ccfdba83f927f
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2022-11-24 11:27:18 -0300

    SimpleCalculator

---
+++ README.md
@@ -0,0 +1,25 @@
+﻿# SimpleCalculator
+
+This project was created using [PyBridge](https://github.com/hbisneto/PyBridge)
+
+## Requirements
+
+SimpleCalculator requires Python 3.9.0 or later to run
+
+## Installation
+
+```
+pip install SimpleCalculator
+```
+
+## External Links
+
+Here is some external links that you can use in your `README.md` file.
+
+- [First Link](https://google.com)
+- [Second Link](https://google.com)
+- [Third Link](https://google.com)
+
+#
+
+Copyright © 2022 Target. All rights reserved.
---
+++ linux/LinuxApp.py
@@ -0,0 +1,61 @@
+﻿## LinuxApp File
+## This file is used to implement code used to run scripts for Linux
+
+from exception import Exceptions
+from linux import FileSystem
+
+def Main():
+   # Obtém o primeiro número
+   print("=" * 80)
+   print(">> ENTRADA DO USUÁRIO <<")
+   print("=" * 80)
+   N1 = int(input("Insira o primeiro número: "))
+   print("=" * 80)
+   print("\n")
+
+
+   # Obtém o segundo número
+
+   # Informa o usuário sobre as operações disponíveis
+   print("=" * 80)
+   print("Operações disponíveis")
+   print("=" * 80)
+   print(">> [+]: Soma")
+   print(">> [-]: Subrtração")
+   print(">> [*]: Multiplicação")
+   print(">> [/]: Divisão")
+
+   # Obtém a operação
+   print("=" * 80)
+   print(">> ENTRADA DO USUÁRIO <<")
+   print("=" * 80)
+   Result = str(input("Insira uma operação: "))
+   print("=" * 80)
+   print("\n")
+
+   # Obtém o segundo número
+   print("=" * 80)
+   print(">> ENTRADA DO USUÁRIO <<")
+   print("=" * 80)
+   N2 = int(input("Insira o segundo número: "))
+   print("=" * 80)
+
+   # Executa o código condizente com a operação escolhida pelo usuário
+   if Result == "+":
+       Result = N1 + N2
+   elif Result == "-":
+       Result = N1 - N2
+   elif Result == "*":
+       Result = N1 * N2
+   elif Result == "/":
+       Result = N1 / N2
+   else:
+       Result = "Indisponível"
+       print("Não foi possível realizar o cálculo. Verifique a operação.")
+
+   # Imprime o resultado
+   print("[Resultado]:", Result)
+   print("=" * 80)
+
+
+Main()
